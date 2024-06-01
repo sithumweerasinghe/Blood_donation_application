fa763b345772af95b7119cf6c4a41bb2a185e75f
sithumweerasinghe <sithumweerasinghe01@gmail.com>
2024-06-01 11:32:14 +0530

    welcome text

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -7,7 +7,6 @@ class WelcomePage extends StatelessWidget {
     'assets/images/blood1.jpg',
     'assets/images/blood2.png',
     'assets/images/blood3.jpg',
-    'assets/images/blood2.png',
   ];
 
 
